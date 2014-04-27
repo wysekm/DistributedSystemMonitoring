65175eaa4c765f810964fb251a45e80cc20e50ea
przemekg <@>
2014-04-27 14:18:37 +0200

    space indent -> tab indent

--- DSM-common/src/test/java/pl/edu/agh/dsm/common/measurements/MeasurementRepositoryTest.java
+++ DSM-common/src/test/java/pl/edu/agh/dsm/common/measurements/MeasurementRepositoryTest.java
@@ -16,53 +16,53 @@ import java.util.UUID;
 
 public class MeasurementRepositoryTest {
 
-    MeasurementRepository measurementRepository;
-    MeasurementDto measurementDto;
+	MeasurementRepository measurementRepository;
+	MeasurementDto measurementDto;
 
-    @Before
-    public void setUp() {
-        measurementRepository = new MeasurementRepositoryImpl();
-        measurementDto = addDto();
-    }
+	@Before
+	public void setUp() {
+		measurementRepository = new MeasurementRepositoryImpl();
+		measurementDto = addDto();
+	}
 
-    @Test
-    public void testAdd() throws IOException {
-        measurementRepository.save(measurementDto);
-        Assert.assertEquals(measurementDto, measurementRepository.find(measurementDto.getId()));
-    }
+	@Test
+	public void testAdd() throws IOException {
+		measurementRepository.save(measurementDto);
+		Assert.assertEquals(measurementDto, measurementRepository.find(measurementDto.getId()));
+	}
 
-    @Test
-    public void testDelete() throws IOException {
-        Predicate<MeasurementDto> precondition = Predicates.alwaysTrue();
-        List<MeasurementDto> listOfMeasurements = measurementRepository.findAll(precondition);
-        Assert.assertTrue(listOfMeasurements.contains(measurementDto));
+	@Test
+	public void testDelete() throws IOException {
+		Predicate<MeasurementDto> precondition = Predicates.alwaysTrue();
+		List<MeasurementDto> listOfMeasurements = measurementRepository.findAll(precondition);
+		Assert.assertTrue(listOfMeasurements.contains(measurementDto));
 
-        measurementRepository.remove(measurementDto.getId());
-        listOfMeasurements = measurementRepository.findAll(precondition);
-        Assert.assertFalse(listOfMeasurements.contains(measurementDto));
-    }
+		measurementRepository.remove(measurementDto.getId());
+		listOfMeasurements = measurementRepository.findAll(precondition);
+		Assert.assertFalse(listOfMeasurements.contains(measurementDto));
+	}
 
-    @Test
-    public void testGetList() throws IOException {
-        Predicate<MeasurementDto> precondition = Predicates.alwaysTrue();
+	@Test
+	public void testGetList() throws IOException {
+		Predicate<MeasurementDto> precondition = Predicates.alwaysTrue();
 
-        MeasurementDto measurementDto1 = addDto();
-        MeasurementDto measurementDto2 = addDto();
-        MeasurementDto measurementDto3 = addDto();
+		MeasurementDto measurementDto1 = addDto();
+		MeasurementDto measurementDto2 = addDto();
+		MeasurementDto measurementDto3 = addDto();
 
-        List<MeasurementDto> listOfMeasurements = measurementRepository.findAll(precondition);
+		List<MeasurementDto> listOfMeasurements = measurementRepository.findAll(precondition);
 
-        Assert.assertTrue(listOfMeasurements.contains(measurementDto1));
-        Assert.assertTrue(listOfMeasurements.contains(measurementDto2));
-        Assert.assertTrue(listOfMeasurements.contains(measurementDto3));
+		Assert.assertTrue(listOfMeasurements.contains(measurementDto1));
+		Assert.assertTrue(listOfMeasurements.contains(measurementDto2));
+		Assert.assertTrue(listOfMeasurements.contains(measurementDto3));
 
-    }
+	}
 
-    public MeasurementDto addDto() {
-        MeasurementDto mDto = new MeasurementDto(UUID.randomUUID(), "Host", "memUsage", "mb", "http://localhost:8085");
-        measurementRepository.save(mDto);
-        return mDto;
-    }
+	public MeasurementDto addDto() {
+		MeasurementDto mDto = new MeasurementDto(UUID.randomUUID(), "Host", "memUsage", "mb", "http://localhost:8085");
+		measurementRepository.save(mDto);
+		return mDto;
+	}
 
 
 }
