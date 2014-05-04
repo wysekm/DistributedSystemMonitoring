4cc8dd97c0f93ab9ef421124c1a7e80e9e9b1dfb
Burt <b.rupik@gmail.com>
2014-05-04 15:43:50 +0200

    #34 Nowe pomiary sensora

--- DSM-sensor/src/main/java/pl/edu/agh/dsm/sensor/monitored/MonitoredFactory.java
+++ DSM-sensor/src/main/java/pl/edu/agh/dsm/sensor/monitored/MonitoredFactory.java
@@ -8,6 +8,14 @@ public class MonitoredFactory {
 			throws MonitoringException {
 		if (MonitoredCpu.METRIC_NAME.equals(metricName)) {
 			return new MonitoredCpu();
+		} else if (MonitoredActualMem.METRIC_NAME.equals(metricName)) {
+			return new MonitoredActualMem();
+		} else if (MonitoredActualFreeMem.METRIC_NAME.equals(metricName)) {
+			return new MonitoredActualFreeMem();
+		} else if (MonitoredPercentageFreeMem.METRIC_NAME.equals(metricName)) {
+			return new MonitoredPercentageFreeMem();
+		} else if (MonitoredPercentageUsedMem.METRIC_NAME.equals(metricName)) {
+			return new MonitoredPercentageUsedMem();
 		} else {
 			throw new MonitoringException("Unknown metric: " + metricName);
 		}
---
+++ DSM-sensor/src/main/java/pl/edu/agh/dsm/sensor/monitored/impl/MonitoredActualFreeMem.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.dsm.sensor.monitored.impl;
+
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import pl.edu.agh.dsm.sensor.monitored.MonitoredResource;
+import pl.edu.agh.dsm.sensor.monitored.MonitoringException;
+
+public class MonitoredActualFreeMem extends MonitoredResource {
+
+	public static final String METRIC_NAME = "actualFreeMem";
+
+	public MonitoredActualMem() {
+		super(METRIC_NAME);
+	}
+
+	@Override
+	public double checkValue() throws MonitoringException {
+		try {
+			return new Sigar().getMem().getActualFree().doubleValue();
+		} catch (SigarException ex) {
+			throw new MonitoringException(
+					"SigarException caught while performing resource check: ",
+					ex
+			);
+		}
+	}
+
+	@Override
+	public String getUnit() {
+		return "MB";
+	}
+
+}
