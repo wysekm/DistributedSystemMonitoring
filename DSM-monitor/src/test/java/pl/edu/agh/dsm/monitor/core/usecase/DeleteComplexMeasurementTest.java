a9a8f20507cfe1d6bd47e7976261a8ad144d7a0a
Jan Kleszczyński <9kleszcz@fatcat.ftj.agh.edu.pl>
2014-06-04 15:12:47 +0200

    Delete measurement use case test

--- DSM-monitor/src/test/java/pl/edu/agh/dsm/monitor/core/usecase/DeleteComplexMeasurementTest.java
+++ DSM-monitor/src/test/java/pl/edu/agh/dsm/monitor/core/usecase/DeleteComplexMeasurementTest.java
@@ -1,9 +1,12 @@
 package pl.edu.agh.dsm.monitor.core.usecase;
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
 import java.util.UUID;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +25,55 @@ import pl.edu.agh.dsm.monitor.core.model.user.ApplicationUser;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = MonitorConfig.class)
 @ActiveProfiles("mockComponents")
-public class CreateComplexMeasurementTest {
-
+public class DeleteComplexMeasurementTest {
+	
+	ApplicationUser user;
+	Measurement measurement;
+	ComplexMeasurement complexMeasurement;
+	
+	
 	@Autowired
-	CreateComplexMeasurement uut;
+	DeleteComplexMeasurement uut;
 	@Autowired
 	AddSensorMeasurement addSensor;
 	
-	ApplicationUser user;
-	ComplexMeasurement complexMeasurement;
+	@Autowired
+	CreateComplexMeasurement createComplex;
 	
 	@Test
-	public void testCreate() {
+	public void testDelete() {
+		uut.delete(complexMeasurement.getId(), complexMeasurement.getCreatedBy());
+		ActionPossibility ap = uut.measurementExists(complexMeasurement.getId());
+		assertFalse(ap.getReason(), ap.isPossible());
+	}
+
+	@Test
+	public void testCanDelete() {
+		
+		ApplicationUser newUser = new ApplicationUser("");
+		ActionPossibility ap = uut.canDelete(complexMeasurement.getId(), newUser);
+		Assert.assertFalse(ap.getReason(), ap.isPossible());
+		user.setName("TestUser");
+		 ap = uut.canDelete(complexMeasurement.getId(), user);
+		Assert.assertTrue(ap.getReason(), ap.isPossible());
+	}
+
+	@Test
+	public void testMeasurementExists() {
+		
+		UUID uuid = UUID.fromString("0cb419a8-aa9b-41ba-ad33-4869eaa658ff");
+		ActionPossibility ap = uut.measurementExists(uuid);
+		assertFalse(ap.isPossible());
+		ap = uut.measurementExists(complexMeasurement.getId());
+		assertTrue(ap.getReason(), ap.isPossible());
+
+	}
+	
+	@Before
+	public void prepare() {
 		user = new ApplicationUser("TestUser");
 		
-		
-		UUID mId = UUID.randomUUID();
+		UUID mId = UUID.fromString("0cb419a8-aa9b-41ba-ad33-4869eaa658aa");
 		String resource, metric, unit, monitor;
 		resource = "localhost"; 
 		metric = "cpu";
@@ -47,34 +83,15 @@ public class CreateComplexMeasurementTest {
 		MeasurementData data = new MeasurementData(1000000, 1.0);
 		addSensor.addSensorMeasurement(measurement, data);
 		
-		UUID cmId = UUID.randomUUID();
+		UUID cmId = UUID.fromString("0cb419a8-aa9b-41ba-ad33-4869eaa658bb");
 				
 		complexMeasurement = new ComplexMeasurement(cmId, mId, "move_avg", Arrays.asList(
 				new ComplexMeasurementParam("interval", 15),
 				new ComplexMeasurementParam("time_window", 20)), user);
 		
-		Measurement cm = uut.create(complexMeasurement, user);
-		Assert.assertNotNull(cm);
+		createComplex.create(complexMeasurement, user);
 	}
-
-	@Test
-	public void testCanCreate() {
-		user = new ApplicationUser("");
-		Assert.assertFalse(uut.canCreate(user).isPossible());
-		user.setName("TestUser");
-		Assert.assertTrue(uut.canCreate(user).isPossible());
-	}
-
-	@Test
-	public void testBaseMeasurementExists() {
-		user = new ApplicationUser("TestUser");
-		complexMeasurement = new ComplexMeasurement(null, UUID.randomUUID(), "move_avg", Arrays.asList(
-				new ComplexMeasurementParam("interval", 15),
-				new ComplexMeasurementParam("time_window", 20)), user);
-		
-		ActionPossibility pos = uut.baseMeasurementExists(complexMeasurement);
-		Assert.assertNotNull(pos);
-		Assert.assertFalse(pos.isPossible());
-	}
+	
+	
 
 }
